cf848b48e499f226da3d9d2802e3176281cf152b
Devin Spivey <dspivey@doozer.com>
2016-11-05 14:54:41 -0500

    added a call to the CheckDatabase function to verify that we can connect to the database when the application is started.

--- src/todo/todo.go
+++ src/todo/todo.go
@@ -4,18 +4,27 @@ import (
 	"fmt"
 	"net/http"
 	"todo/controller"
+	"todo/log"
 	"todo/middleware"
+	"todo/model"
 	"todo/router"
 )
 
 func main() {
-	controller.RegisterRoutes()
+	// check if database exists
+	err := model.CheckDatabase()
+	if err != nil {
+		log.Fatal("Error connecting to database:", err)
+	}
 
-	// wrap our router with middleware to log each request
-	handler := middleware.LogRequest(router.Router())
+	// register all controller routes
+	controller.RegisterRoutes()
 
 	// handle static assets (css, js, etc.)
 	router.HandleStatic()
+
+	// wrap our router with middleware to log each request
+	handler := middleware.LogRequest(router.Router())
 
 	fmt.Print("Listening on port 8000...\n")
 	http.ListenAndServe(":8000", handler)
